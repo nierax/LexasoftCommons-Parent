5588495f0a0f3263935da7a1b018e4d8a906a39e
nierax <axel.niering@lexasoft.de>
2021-02-06 14:20:21 +0100

    Removed JUnit4 annotations, as they are not needed in context of JUnit 5

--- ls-commons/src/test/java/de/lexasoft/common/cli/ConsoleValidatorTest.java
+++ ls-commons/src/test/java/de/lexasoft/common/cli/ConsoleValidatorTest.java
@@ -12,19 +12,16 @@ import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import de.lexasoft.common.model.Validator;
 
 /**
- * @author Axel
+ * @author nierax
  *
  */
 @ExtendWith(MockitoExtension.class)
-@RunWith(JUnitPlatform.class)
 class ConsoleValidatorTest {
 
   ConsoleValidator cut;
