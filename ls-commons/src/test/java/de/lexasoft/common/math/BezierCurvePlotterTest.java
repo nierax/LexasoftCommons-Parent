a09be73cf43bb1501d24e4dc24ec49356bc05d7e
nierax <axel.niering@lexasoft.de>
2021-05-13 19:29:38 +0200

    Make sure, directory exists, when test is running

--- ls-commons/src/test/java/de/lexasoft/common/math/BezierCurvePlotterTest.java
+++ ls-commons/src/test/java/de/lexasoft/common/math/BezierCurvePlotterTest.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -26,6 +26,8 @@ import org.junit.jupiter.params.provider.MethodSource;
  *
  */
 class BezierCurvePlotterTest {
+
+	private final static String DIRECTORY = "junit-tmp";
 
 	private static List<Point> create4PointList() {
 		List<Point> list = new ArrayList<>();
@@ -55,19 +57,28 @@ class BezierCurvePlotterTest {
 	/**
 	 * @throws java.lang.Exception
 	 */
-	@BeforeEach
-	void setUp() throws Exception {
+	@BeforeAll
+	static void initialize() throws Exception {
+		File dir = new File(DIRECTORY);
+		if (!dir.exists()) {
+			dir.mkdir();
+		}
 	}
 
 	private static Stream<Arguments> testPlotCurve() {
-		return Stream.of(Arguments.of(create4PointList(), "junit-tmp/4-point-list.png"),
-		    Arguments.of(create3PointList(), "junit-tmp/3-point-list.png"),
-		    Arguments.of(createList(Point.of(0, 0), Point.of(75, 75), Point.of(100, 100)), "junit-tmp/3-point-75-75.png"),
-		    Arguments.of(createList(Point.of(0, 0), Point.of(75, 100), Point.of(100, 100)), "junit-tmp/3-point-75-100.png"),
-		    Arguments.of(createList(Point.of(0, 0), Point.of(66, 100), Point.of(100, 100)), "junit-tmp/3-point-66-100.png"),
-		    Arguments.of(createList(Point.of(0, 0), Point.of(66, 120), Point.of(100, 100)), "junit-tmp/3-point-66-120.png"),
-		    Arguments.of(createList(Point.of(0, 0), Point.of(50, 100), Point.of(100, 100)), "junit-tmp/3-point-50-100.png"),
-		    Arguments.of(createList(Point.of(0, 0), Point.of(50, 0), Point.of(100, 100)), "junit-tmp/3-point-50-0.png"));
+		return Stream.of(Arguments.of(create4PointList(), DIRECTORY + "/4-point-list.png"),
+		    Arguments.of(create3PointList(), DIRECTORY + "/3-point-list.png"),
+		    Arguments.of(createList(Point.of(0, 0), Point.of(75, 75), Point.of(100, 100)),
+		        DIRECTORY + "/3-point-75-75.png"),
+		    Arguments.of(createList(Point.of(0, 0), Point.of(75, 100), Point.of(100, 100)),
+		        DIRECTORY + "/3-point-75-100.png"),
+		    Arguments.of(createList(Point.of(0, 0), Point.of(66, 100), Point.of(100, 100)),
+		        DIRECTORY + "/3-point-66-100.png"),
+		    Arguments.of(createList(Point.of(0, 0), Point.of(66, 120), Point.of(100, 100)),
+		        DIRECTORY + "/3-point-66-120.png"),
+		    Arguments.of(createList(Point.of(0, 0), Point.of(50, 100), Point.of(100, 100)),
+		        DIRECTORY + "/3-point-50-100.png"),
+		    Arguments.of(createList(Point.of(0, 0), Point.of(50, 0), Point.of(100, 100)), DIRECTORY + "/3-point-50-0.png"));
 	}
 
 	@ParameterizedTest
