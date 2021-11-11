b51fe669722bc53b21c1797bdf2cbbcf063c4466
nierax <axel.niering@lexasoft.de>
2021-11-11 20:02:55 +0100

    Added packages to module-info.java

--- ls-commons/src/main/java/module-info.java
+++ ls-commons/src/main/java/module-info.java
@@ -1,5 +1,8 @@
-module de.lexasoft {
+module de.lexasoft.common {
 	requires java.desktop;
 	requires java.logging;
 
+	exports de.lexasoft.common.model;
+	exports de.lexasoft.util;
+	exports de.lexasoft.common.math;
 }
