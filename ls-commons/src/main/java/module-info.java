9ba1adadfa64bda666038848e77cccacd0462c11
nierax <axel.niering@lexasoft.de>
2021-11-15 19:18:34 +0100

    Opened more packages

--- ls-commons/src/main/java/module-info.java
+++ ls-commons/src/main/java/module-info.java
@@ -5,4 +5,10 @@ module de.lexasoft.common {
 	exports de.lexasoft.common.model;
 	exports de.lexasoft.util;
 	exports de.lexasoft.common.math;
+
+	opens de.lexasoft.common.cli;
+	opens de.lexasoft.common.math;
+	opens de.lexasoft.common.model;
+	opens de.lexasoft.common.swing;
+	opens de.lexasoft.util;
 }
